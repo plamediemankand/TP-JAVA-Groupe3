e71b934817f78d60ad9bac6c24688f03a83a11d1
kapafulecornelie <kapafulecornelie00@gmail.com>
2026-02-09 15:41:06 +0200

    Étudiant ilungamyria,: Ajout MenuManager

---
+++ src/MenuManager.java
@@ -0,0 +1,21 @@
+public class MenuManager {
+    public static void ajouterOperateur() {
+        System.out.println("Appel du module Ajouter Opérateur...");
+        // Appel à la classe de l'Équipe B
+    }
+    
+    public static void enregistrerProduction() {
+        System.out.println("Appel du module Enregistrement Production...");
+        // Appel à la classe de l'Équipe C
+    }
+    
+    public static void afficherProductionDuJour() {
+        System.out.println("Appel du module Affichage Production du Jour...");
+        // Appel à la classe de l'Équipe D
+    }
+    
+    public static void genererRapportDuJour() {
+        System.out.println("Appel du module Génération Rapport...");
+        // Appel à la classe de l'Équipe E
+    }
+}
